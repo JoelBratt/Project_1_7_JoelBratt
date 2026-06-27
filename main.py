5b8a20d8e513dc1187b1a25a56b5bdc2ee039e42
Joel Bratt <jbratt1@student.cscc.edu>
2026-06-26 20:43:58 -0400

    made the grid work and input for first position

--- main.py
+++ main.py
@@ -13,4 +13,10 @@ xORo = input("Lets play Tic Tac Toe would you like to be O's or X's? ")
 while xORo != "X" and xORo != "O":
     xORo = input("That wasn't X or O input those and make sure they're capital! ")
 else: print('Great lets move on')
-print(xORo)
+print(f' {spots[0]} | {spots[1]} | {spots[2]} \n'
+      f'----------\n'
+      f' {spots[3]} | {spots[4]} | {spots[5]} \n'
+      f'----------\n'
+      f' {spots[6]} | {spots[7]} | {spots[8]} ')
+print("pick a number 1-9: ")
+pick = input((int))
