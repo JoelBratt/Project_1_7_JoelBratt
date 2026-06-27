e5c588c8ff0f47bfe0c6b8f643671ed845e14ed4
Joel Bratt <jbratt1@student.cscc.edu>
2026-06-26 20:24:35 -0400

    Added X or O check for game idea

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ the main program tbd
 """
 import math
 import random
+
+spots = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+
+xORo = input("Lets play Tic Tac Toe would you like to be O's or X's? ")
+while xORo != "X" and xORo != "O":
+    xORo = input("That wasn't X or O input those and make sure they're capital! ")
+else: print('Great lets move on')
+print(xORo)
